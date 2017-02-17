e47424f645a40c97ad4e7c51107c91164d191251
Clark Gaylord <cgaylord@vt.edu>
2017-02-17 17:39:46 -0500

    create the addrwatch_log table update/insert from addrwatch to addrwatch_log

---
+++ addrwatch_log_create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE addrwatch_log
+  (mac_address varchar(17),
+   ip_address  varchar(42),
+   insert_time unsigned big int,
+   update_time unsigned big int);
+
---
+++ update_log.sql
@@ -0,0 +1,29 @@
+begin transaction;
+update addrwatch_log
+   set update_time = (
+         select max(timestamp)
+           from addrwatch as a
+          where a.mac_address = addrwatch_log.mac_address and
+                a.ip_address = addrwatch_log.ip_address
+          group by mac_address, ip_address
+       )
+ where exists (
+         select max(timestamp)
+           from addrwatch as a
+          where a.mac_address = addrwatch_log.mac_address and
+                a.ip_address = addrwatch_log.ip_address and
+                a.timestamp > addrwatch_log.update_time
+          group by mac_address, ip_address
+         )
+;
+insert into addrwatch_log
+select a.mac_address, a.ip_address, a.timestamp, a.timestamp
+  from addrwatch as a
+ where not exists (
+       select l.mac_address, l.ip_address
+         from addrwatch_log as l
+        where l.mac_address = a.mac_address and
+              l.ip_address  = a.ip_address)
+;
+delete from addrwatch;
+commit;
